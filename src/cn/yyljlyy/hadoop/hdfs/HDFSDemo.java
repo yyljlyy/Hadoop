f90e618060a95dc3d9086d11a239f453ee8a98f7
yyljlyy <fkhacker@vip.qq.com>
2014-12-15 17:40:18 +0800

    更改版
    空指针状况

--- src/cn/yyljlyy/hadoop/hdfs/HDFSDemo.java
+++ src/cn/yyljlyy/hadoop/hdfs/HDFSDemo.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -16,27 +15,44 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class HDFSDemo {
-	FileSystem fs =null;
+	FileSystem fs = null;
+
 	@Before
-	public void init() throws Exception{
-		FileSystem fs = FileSystem.get(new URI("hdfs://hadoop01.yyljlyy:9000"),new Configuration());
+	public void init() throws Exception {
+		FileSystem fs = FileSystem.get(new URI("hdfs://hadoop01.yyljlyy:9000"),new Configuration(),"root");
 	}
+
 	@Test
-	public void testUpload() throws Exception{
-		//读取本地文件系统的文件
+	public void testUpload() throws Exception {
+		// 读取本地文件系统的文件
 		FileInputStream in = new FileInputStream("e://test.txt");
 		OutputStream out = fs.create(new Path("/test.txt"));
-		IOUtils.copyBytes(in, out, 4096,true);
+		IOUtils.copyBytes(in, out, 4096, true);
 	}
 	@Test
-	public void testMkdir(){
-		
-	}	
+	public void testDownload() throws Exception{
+		FileSystem fs = FileSystem.get(new URI("hdfs://hadoop01.yyljlyy:9000"),new Configuration(),"root");
+		fs.copyFromLocalFile(new Path("/jdk1.7"), new Path("e://jdk"));
+	}
+	@Test
+	public void testDel() throws Exception {
+		FileSystem fs = FileSystem.get(new URI("hdfs://hadoop01.yyljlyy:9000"),new Configuration(),"root");
+		boolean flag = fs.delete(new Path("/test.txt"), false);
+		System.out.println(flag);
+	}
+
+	@Test
+	public void testMkdir() throws Exception {
+		FileSystem fs = FileSystem.get(new URI("hdfs://hadoop01.yyljlyy:9000"),new Configuration(),"root");
+		boolean flag = fs.mkdirs(new Path("/die"));
+		System.out.println(flag);
+	}
+
 	public static void main(String[] args) throws Exception {
 		FileSystem fs = FileSystem.get(new URI("hdfs://hadoop01.yyljlyy:9000"),
 				new Configuration());
 		InputStream in = fs.open(new Path("/jdk1.7"));
 		OutputStream out = new FileOutputStream("e://jdk1.7");
 		IOUtils.copyBytes(in, out, 4096, true);
- 	}
+	}
 }
